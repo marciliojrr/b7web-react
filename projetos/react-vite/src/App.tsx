649017301295c4226156000a10759ef524cef264
Marcilio <marciliojr@outlook.com>
2022-08-31 17:41:44 -0300

    Usando states

--- projetos/react-vite/src/App.tsx
+++ projetos/react-vite/src/App.tsx
@@ -1,12 +1,18 @@
+import { useState } from 'react'
+
 const App = () => {
 
+    const [numero, setNumero] = useState(0);
+    const [name, setName] = useState('Marcilio')
+
     const handleButtonClick = () => {
-        alert('O botao foi clicado!')
+        setName('Junior')
     }
 
     return (
         <div>
-            <button onClick={handleButtonClick}>Clique aqui</button>
+            Meu nome: {name}.<br />
+            <button onClick={handleButtonClick}>Clique aqui</button><br />
             <button onClick={() => { alert('O segundo botao foi clicado!') }}>Clique aqui 2</button>
         </div>
     )
