2a4fcc36727ed69708ca4d43821c1b006e3d7527
Marcilio <marciliojr@outlook.com>
2022-08-31 16:54:46 -0300

    Passando props

--- projetos/react-vite/src/App.tsx
+++ projetos/react-vite/src/App.tsx
@@ -3,6 +3,8 @@ import { Header } from "./components/Header";
 const App = () => {
     return (
         <div>
+            <Header title='Example' />
+            <Header title='Example 2' />
             <Header />
             Hello World.
         </div>
--- projetos/react-vite/src/components/Header.tsx
+++ projetos/react-vite/src/components/Header.tsx
@@ -1,7 +1,13 @@
-export const Header = () => {
+type Props = {
+    title?: string // ? = opcional
+}
+
+//export const Header = (props: Props) => {
+export const Header = ({ title }: Props) => { // forma mais usada
     return (
         <header>
-            <h1>Header</h1>
+            {/* <h1>{props.title}</h1> */}
+            <h1>{title}</h1>
             <hr />
         </header>
     )
