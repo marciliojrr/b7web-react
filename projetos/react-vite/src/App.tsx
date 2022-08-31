47458ebbea276549c8e65cd80e27e43fb6e6020d
Marcilio <marciliojr@outlook.com>
2022-08-31 16:46:09 -0300

    Usando mais de um componente

--- projetos/react-vite/src/App.tsx
+++ projetos/react-vite/src/App.tsx
@@ -1,17 +1,11 @@
+import { Header } from "./components/Header";
+
 const App = () => {
-
-    let name: string = 'marcilio';
-    let lastName: string = 'junior'
-    let link: string = 'https://www.google.com.br'
-
     return (
         <div>
-            Nome: {`${name} ${lastName}`}<br />
-            Link para o site: <br />
-            <a href={link}>Clique aqui para acessar o Google</a>
+            <Header />
+            Hello World.
         </div>
-
-
     )
 }
 
