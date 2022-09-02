a1d749e60b8519da292f91c80bbeb5ed2322beea
Marcilio <marciliojr@outlook.com>
2022-09-01 21:48:54 -0300

    Renderizando listas

--- projetos/react-vite/src/App.tsx
+++ projetos/react-vite/src/App.tsx
@@ -1,16 +1,22 @@
-import { Button } from "./components/Button";
+import { Pessoa } from "./components/Pessoa";
 
 const App = () => {
 
-    let buttonText = 'Clique aqui'
-
-    const eventButtonAction = (txt: string) => {
-        alert(`Frase do App.tsx -> ${txt}`)
-    }
+    let list = [
+        { name: 'Marcilio Junior', age: 29 },
+        { name: 'Michelly', age: 23 },
+        { name: 'Edilza', age: 50 },
+        { name: 'Marcilio', age: 51 }
+    ]
 
     return (
         <div>
-            <Button text={buttonText} clickFn={eventButtonAction} />
+            <h2>Lista de presenca</h2>
+            <ul>
+                {list.map((item, index) => (
+                    <Pessoa key={index} data={item} />
+                ))}
+            </ul>
         </div>
     )
 }
