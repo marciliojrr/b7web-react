6e43924600ac3d9332f4d868e4bcbb9f487b6985
Marcilio <marciliojr@outlook.com>
2022-09-02 13:37:15 -0300

    Estilizando filhos do componente

--- projetos/react-vite/src/App.tsx
+++ projetos/react-vite/src/App.tsx
@@ -4,7 +4,8 @@ const App = () => {
 
     return (
         <C.Container bg='#1b1ba49d'>
-            But by default, she don't think on me...
+            <span>But by default, she don't think on me... </span>
+            <a href='' className='link'>Link qualquer </a>
             <C.Button bg='#FF0000'>Botão grande</C.Button>
             <C.Button bg='#00FF00' small>Botão pequeno</C.Button>
         </C.Container>
--- projetos/react-vite/src/AppStyles.ts
+++ projetos/react-vite/src/AppStyles.ts
@@ -7,6 +7,19 @@ export const Container = styled.div<ContainerProps>`
     background-color: ${props => props.bg};
     color: white;
     padding: 20px;
+
+    .link {
+        color: #FFF;
+
+        &:hover{
+            color: #00FF00;
+        }
+    }
+
+    span {
+        font-weight: bold;
+        color: #000;
+    }
 `;
 
 type ButtonProps = {
@@ -16,5 +29,5 @@ type ButtonProps = {
 export const Button = styled.button <ButtonProps>`
     font-size: ${props => props.small ? '15px' : '30px'};
     background-color: ${props => props.bg};
-    border-radius: 10px;
+    border-radius: 5px;
 `;
