7f7908480532da45ccfe55e4ba49e0d621a574c1
Marcilio <marciliojr@outlook.com>
2022-09-02 13:00:37 -0300

    Criando componentes com Styled Components

--- projetos/react-vite/src/App.tsx
+++ projetos/react-vite/src/App.tsx
@@ -1,11 +1,25 @@
+// import { Container, Button } from "./AppStyles"; // ---> [1] <---
+import * as C from './AppStyles' // ---> [2] <----
 
+// ---> [1] <---
+// const App = () => {
 
+//     return (
+//         <Container>
+//             But by default, she don't think on me...
+//             <Button>Clique aqui</Button>
+//         </Container>
+//     )
+// }
+
+// ---> [2] <----
 const App = () => {
 
     return (
-        <div>
-            ...
-        </div>
+        <C.Container>
+            But by default, she don't think on me...
+            <C.Button>Clique aqui</C.Button>
+        </C.Container>
     )
 }
 
