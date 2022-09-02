01c983814bd9a9d005ccfca824d80915a99d420d
Marcilio <marciliojr@outlook.com>
2022-09-01 22:21:14 -0300

    Estilizando inline

--- projetos/react-vite/src/App.tsx
+++ projetos/react-vite/src/App.tsx
@@ -1,21 +1,10 @@
-import { useState } from "react";
-
 const App = () => {
 
-    const [show, setShow] = useState(false)
 
-    const handleClick = () => {
-        setShow(!show)
-    }
 
     return (
         <div>
-            <button onClick={handleClick}>{show ? 'Ocultar' : 'Mostrar'}</button>
-            {show &&
-                <div>
-                    Xinforinfola
-                </div>
-            }
+            <button style={{ backgroundColor: '#FF0000', color: '#FFF', border: 0 }}>Clique aqui</button>
         </div>
     )
 }
