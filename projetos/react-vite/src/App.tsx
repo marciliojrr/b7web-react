b2906fd16211e6f25cd63bfa9df089cbe45f280a
Marcilio <marciliojr@outlook.com>
2022-09-03 17:10:12 -0300

    Responsividade no TailwindCSS

--- projetos/react-vite/src/App.tsx
+++ projetos/react-vite/src/App.tsx
@@ -1,10 +1,10 @@
 const App = () => {
 
     return (
-        <div className="bg-blue-900 text-white p-4 text-2xl font-bold">
+        <div className="bg-blue-900 text-white p-4 text-2xl font-bold flex flex-col md:flex-row md:text-green-500 lg:text-black  md:justify-between text-center">
             But by default, she don't think on me...
 
-            <button className="bg-green-300 text-black m-2 p-2 rounded-xl">Clique aqui</button>
+            <button className="bg-green-300 text-black m-2 md:ml-3 p-2 rounded-xl">Clique aqui</button>
         </div>
     )
 }
