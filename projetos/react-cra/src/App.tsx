0e81cd8f01a23cb656f62dea353678f1a621ebc7
Marcilio <marciliojr@outlook.com>
2022-09-03 16:57:46 -0300

    Usando TailwindCSS na prática e novo proj CRA

---
+++ projetos/react-cra/src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+    return (
+        <div>
+            But by default, she don't think on me...
+        </div>
+    );
+}
+
+export default App;
--- projetos/react-vite/src/App.tsx
+++ projetos/react-vite/src/App.tsx
@@ -1,8 +1,10 @@
 const App = () => {
 
     return (
-        <div className="bg-black">
+        <div className="bg-blue-900 text-white p-4 text-2xl font-bold">
             But by default, she don't think on me...
+
+            <button className="bg-green-300 text-black m-2 p-2 rounded-xl">Clique aqui</button>
         </div>
     )
 }
